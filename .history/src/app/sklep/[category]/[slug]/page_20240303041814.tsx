74867bd262e051f44bfb8de0077cdf8ac3f0d113
wesselpawel <wesselpawel@wp.pl>
2024-03-03 04:19:23 +0100

    shop improvement

--- .history/src/app/sklep/[category]/[slug]/page_20240303041814.tsx
+++ .history/src/app/sklep/[category]/[slug]/page_20240303041814.tsx
@@ -305,14 +305,20 @@ export async function generateMetadata({ params }: { params: any }) {
   if (product)
     return {
       keywords: `naklejki ${product.categories[0]}, naklejki ${product.categories[0]} ręcznie wycinane, naklejki ${product.categories[0]} na każdą okazję, naklejki ${product.categories[0]} na ścianę, naklejki ${product.categories[0]} dla dzieci, naklejki, naklejki bajkowe, naklejki złote, naklejki ${product.categories[0]} holograficzne, naklejki ${product.categories[0]} srebrne, drukowanie naklejek, naklejki z anime, naklejki na ścianę do kuchni, naklejki na ścianę nowoczesne, naklejki na ścianę dinozaury, naklejki na ścianę kwiaty, nalepki ${product.categories[0]} na ścianę, wlepki, nalepki, wlepy, nakejki, nakleki, nalejki`,
-      title: `Zaklejki.pl: Sklep z Naklejkami - ${product.title}`,
+      title: `Zaklejki.pl: Sklep z Naklejkami - ${removeNumbersFromString(
+        product.title
+      )}`,
       description: `Ręcznie wycinana ${product.title} - Kup jedną z naszych naklejek i twórz własne. Naklejki złote, srebrne, holo. Naklejki na każdą okazję. Sklep z największą kolekcja naklejek ozdobnych.`,
       openGraph: {
         keywords: `naklejki ${product.categories[0]}, naklejki ${product.categories[0]} ręcznie wycinane, naklejki ${product.categories[0]} na każdą okazję, naklejki ${product.categories[0]} na ścianę, naklejki ${product.categories[0]} dla dzieci, naklejki, naklejki bajkowe, naklejki złote, naklejki ${product.categories[0]} holograficzne, naklejki ${product.categories[0]} srebrne, drukowanie naklejek, naklejki z anime, naklejki na ścianę do kuchni, naklejki na ścianę nowoczesne, naklejki na ścianę dinozaury, naklejki na ścianę kwiaty, nalepki ${product.categories[0]} na ścianę, wlepki, nalepki, wlepy, nakejki, nakleki, nalejki`,
         type: "website",
         url: "https://zaklejki.pl",
-        title: `Zaklejki.pl: Sklep z Naklejkami - ${product.title}`,
-        description: `Ręcznie wycinana ${product.title} - Kup jedną z naszych naklejek i twórz własne. Naklejki złote, srebrne, holo. Naklejki na każdą okazję. Sklep z największą kolekcja naklejek ozdobnych.`,
+        title: `Zaklejki.pl: Sklep z Naklejkami - ${removeNumbersFromString(
+          product.title
+        )}`,
+        description: `Ręcznie wycinana ${removeNumbersFromString(
+          product.title
+        )} - Kup jedną z naszych naklejek i twórz własne. Naklejki złote, srebrne, holo. Naklejki na każdą okazję. Sklep z największą kolekcja naklejek ozdobnych.`,
         siteName: "zaklejki",
         images: [
           {

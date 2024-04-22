0b8c73804ba6ccdd9c84f9335fc66d9020ad3cc4
wesselpawel <wesselpawel@wp.pl>
2024-04-22 05:44:10 +0200

    colors update

--- src/app/(blog)/blog/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Footer from "@/components/Footer";
-
-export default function ShopLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="bg-white overflow-hidden">{children}</div>;
-}
